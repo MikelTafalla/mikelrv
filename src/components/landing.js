fa6297198e715daff1beeac73c6599e2c44f32de
Mikel1991 <mikel362d@gmail.com>
2020-12-27 15:43:50 -0800

    adds photo, changes tying speed and message displayed

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,52 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Typist from "react-typist";
 import "./landing.css"
 
 
 const Fluid = () => {
 
-    const [renderMsg, setRenderMsg] = useState({state: false})
-    
+    const [renderMsg, setRenderMsg] = useState({ state: false })
+
     const onHeaderTyped = () => {
-        setRenderMsg({state: true});
+        setRenderMsg({ state: true });
     }
     return (
         <React.Fragment>
+            <div className="text-center">
+                <img src={require("../assets/images/MikelColumn.png").default} className="rounded" alt="mikel's photo" />
+            </div>
 
-<div className="TypistExample">
-        <Typist
-          className="TypistExample-header"
-          avgTypingDelay={40}
-          startDelay={2000}
-          onTypingDone={() => onHeaderTyped()}
-        >
-        <a href={""}>Welcome to my Portfolio</a>
-        </Typist>
-        <div className="TypistExample-content">
-          {renderMsg.state ? (
-            <Typist
-              className="TypistExample-message"
-              cursor={{ hideWhenDone: true }}
-            >
-              * Easy to style
-              <Typist.Delay ms={1250} />
-              <br />
-              * Easy to customize
-              <Typist.Delay ms={1250} />
-              <br />
-              * Easy to use backp<Typist.Delay ms={500} />sace
-              <Typist.Backspace count={5} delay={1000} />
-              <Typist.Delay ms={750} />
-              space
-              <Typist.Delay ms={1250} />
-              <br />
-              <span>* <a className="flash">docs</a></span>
-              <br />
-              {''}
-            </Typist>
-          ) : null }
-        </div>
-      </div>
+            <div className="Typist">
+                <Typist
+                    className="Typist-header"
+                    avgTypingDelay={50}
+                    startDelay={2000}
+                    onTypingDone={() => onHeaderTyped()}
+                >
+                    <a href={""}>Hi!!</a>
+                </Typist>
+                <div className="Typist-content">
+                    {renderMsg.state ? (
+                        <Typist
+                            className="Typist-message"
+                            avgTypingDelay={50}
+                            cursor={{ hideWhenDone: true }}
+                        >
+                            * My name is Mikel Rodriguez. I'm a Full-Stack Developer and Spanish Linguist with a Master's degree in Iberian Linguistics. Welcome to my personal website!
+                            
+                            <Typist.Delay ms={1250} />
+                            <br />
+                            <span>* <a href={""} className="flash">docs</a></span>
+                            <br />
+                            {''}
+                        </Typist>
+                    ) : null}
+                </div>
+            </div>
         </React.Fragment>
     )
 
