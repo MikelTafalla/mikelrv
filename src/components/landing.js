63780e993b3a4fb4c344b37500572834b5abb023
Mikel1991 <mikel362d@gmail.com>
2020-12-27 11:14:26 -0800

    creates react app and adds react fluid for landing page

---
+++ src/components/landing.js
@@ -0,0 +1,14 @@
+import React from "react"
+import FluidAnimation from "react-fluid-animation";
+
+const fluid = () => {
+    return (
+      <FluidAnimation
+        style={{ height: "100vh" }}
+      />
+    )
+  
+}
+
+export default fluid;
+ 
