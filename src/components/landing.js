6a0967f930402e683811555301bbc510ca3e5f39
Mikel1991 <mikel362d@gmail.com>
2020-12-27 15:00:40 -0800

    deletes fluid animation and uses typist npm for landing page

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,14 +1,56 @@
-import React from "react"
-import FluidAnimation from "react-fluid-animation";
+import React, {useState} from "react";
+import Typist from "react-typist";
+import "./landing.css"
 
-const fluid = () => {
+
+const Fluid = () => {
+
+    const [renderMsg, setRenderMsg] = useState({state: false})
+    
+    const onHeaderTyped = () => {
+        setRenderMsg({state: true});
+    }
     return (
-      <FluidAnimation
-        style={{ height: "100vh" }}
-      />
+        <React.Fragment>
+
+<div className="TypistExample">
+        <Typist
+          className="TypistExample-header"
+          avgTypingDelay={40}
+          startDelay={2000}
+          onTypingDone={() => onHeaderTyped()}
+        >
+        <a href={""}>Welcome to my Portfolio</a>
+        </Typist>
+        <div className="TypistExample-content">
+          {renderMsg.state ? (
+            <Typist
+              className="TypistExample-message"
+              cursor={{ hideWhenDone: true }}
+            >
+              * Easy to style
+              <Typist.Delay ms={1250} />
+              <br />
+              * Easy to customize
+              <Typist.Delay ms={1250} />
+              <br />
+              * Easy to use backp<Typist.Delay ms={500} />sace
+              <Typist.Backspace count={5} delay={1000} />
+              <Typist.Delay ms={750} />
+              space
+              <Typist.Delay ms={1250} />
+              <br />
+              <span>* <a className="flash">docs</a></span>
+              <br />
+              {''}
+            </Typist>
+          ) : null }
+        </div>
+      </div>
+        </React.Fragment>
     )
-  
+
 }
 
-export default fluid;
- 
+export default Fluid;
+
