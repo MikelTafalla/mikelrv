8a05dd338266163c44e53adad608a5e5d077ca70
Mikel1991 <mikel362d@gmail.com>
2020-12-27 16:32:50 -0800

    adds fontawesome and MDBIcons to landing page

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import Typist from "react-typist";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { MDBIcon } from "mdbreact";
 import "./landing.css"
 
 
@@ -14,13 +17,30 @@ const Fluid = () => {
         <React.Fragment>
             <div className="text-center">
                 <img src={require("../assets/images/MikelColumn.png").default} className="rounded" alt="mikel's photo" />
+                <ul className="social-list list-inline py-2 mx-auto">
+                    <li className="list-inline-item">
+                        <a href={"https://www.linkedin.com/in/mikel-rodriguez-villar/"} target="_blank">
+                            <FontAwesomeIcon icon={faLinkedinIn}/>
+                        </a>
+                    </li>
+                    <li className="list-inline-item">
+                        <a href={"https://github.com/MikelTafalla"} target="_blank">
+                            <FontAwesomeIcon icon={faGithub}/>
+                        </a>
+                    </li>
+                    <li className="list-inline-item">
+                        <a href={"https://drive.google.com/file/d/11QkzKWSPEZ5q3N-XW5pd7j_bkxVU2yW6/view?usp=sharing"} target="_blank">
+                            <MDBIcon icon="award" />
+                        </a>
+                    </li>
+            </ul>
+                
             </div>
-
             <div className="Typist">
                 <Typist
                     className="Typist-header"
                     avgTypingDelay={50}
-                    startDelay={2000}
+                    startDelay={250}
                     onTypingDone={() => onHeaderTyped()}
                 >
                     <a href={""}>Hi!!</a>
@@ -34,9 +54,9 @@ const Fluid = () => {
                         >
                             * My name is Mikel Rodriguez. I'm a Full-Stack Developer and Spanish Linguist with a Master's degree in Iberian Linguistics. Welcome to my personal website!
                             
-                            <Typist.Delay ms={1250} />
+                            <Typist.Delay ms={250} />
                             <br />
-                            <span>* <a href={""} className="flash">docs</a></span>
+                            <span>* <a href={""} className="flash">My Work</a></span>
                             <br />
                             {''}
                         </Typist>
