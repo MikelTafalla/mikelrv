a9798656f4ad8a1e944626dd3e9f67f81354cabd
Mikel1991 <mikel362d@gmail.com>
2020-12-30 11:57:45 -0800

    adds image to education section, modifies css for proper results

--- src/components/about.js
+++ src/components/about.js
@@ -27,19 +27,19 @@ const About = () => {
                 <div className="point">.</div>
             </div>
 
-            <div className="tech">
+            <div>
 
                 <div className="card text-center tech">
 
                     <div className="card-body">
                         <h5 className="card-title">Technologies Known</h5>
                         <br></br>
-                        <p className="card-text">
-                            <div class="row">
+                        <div className="card-text">
+                            <div className="row">
                                 <div className="item col-6 col-lg-3 with-margin">
                                     <div className="item-inner">
-                                        <div className="item-icon"><FontAwesomeIcon icon={faHtml5} style={{color: "orangered", fontSize: 30 + "px"}}/><FontAwesomeIcon icon={faCss3} style={{color: "lightskyblue", fontSize: 30 + "px"}}/></div>
-                                        <h4 className="item-title">HTML &amp; CSS</h4>
+                                        <div className="item-icon"><FontAwesomeIcon icon={faHtml5} style={{ color: "orangered", fontSize: 30 + "px" }} /><FontAwesomeIcon icon={faCss3} style={{ color: "lightskyblue", fontSize: 30 + "px" }} /></div>
+                                        <h5 className="item-title">HTML &amp; CSS</h5>
 
                                     </div>
                                     {/*//item-inner*/}
@@ -48,8 +48,8 @@ const About = () => {
 
                                 <div className="item col-6 col-lg-3 with-margin">
                                     <div className="item-inner">
-                                        <div className="item-icon"><FontAwesomeIcon icon={faJsSquare} style={{color: "rgb(218, 203, 36)", fontSize: 30 + "px"}} /></div>
-                                        <h4 className="item-title">Vanilla JavaScript</h4>
+                                        <div className="item-icon"><FontAwesomeIcon icon={faJsSquare} style={{ color: "rgb(218, 203, 36)", fontSize: 30 + "px" }} /></div>
+                                        <h5 className="item-title">Vanilla JavaScript</h5>
                                         <div className="item-desc">knowledge of ES5 and ES6+</div>
                                     </div>
                                     {/*//item-inner*/}
@@ -58,7 +58,7 @@ const About = () => {
                                 <div className="item col-6 col-lg-3 with-margin">
                                     <div className="item-inner">
                                         <div className="item-icon"><img className=" mr-2" src={require("../assets/icons/jquery2.png").default} alt="jQuery" /></div>
-                                        <h4 className="item-title">jQuery</h4>
+                                        <h5 className="item-title">jQuery</h5>
                                     </div>
                                     {/*//item-inner*/}
                                 </div>
@@ -66,8 +66,8 @@ const About = () => {
 
                                 <div className="item col-6 col-lg-3 with-margin">
                                     <div className="item-inner">
-                                        <div className="item-icon"><FontAwesomeIcon icon={faNodeJs} style={{color: "green", fontSize: 30 + "px"}} /></div>
-                                        <h4 className="item-title">Node.js</h4>
+                                        <div className="item-icon"><FontAwesomeIcon icon={faNodeJs} style={{ color: "green", fontSize: 30 + "px" }} /></div>
+                                        <h5 className="item-title">Node.js</h5>
                                         <div className="item-desc">Experience with Express.js</div>
 
                                     </div>
@@ -77,8 +77,8 @@ const About = () => {
 
                                 <div className="item col-6 col-lg-3 with-margin">
                                     <div className="item-inner">
-                                        <div className="item-icon"><FontAwesomeIcon icon={faReact} style={{color: "skyblue", fontSize: 30 + "px"}} /></div>
-                                        <h4 className="item-title">React.js</h4>
+                                        <div className="item-icon"><FontAwesomeIcon icon={faReact} style={{ color: "skyblue", fontSize: 30 + "px" }} /></div>
+                                        <h5 className="item-title">React.js</h5>
 
                                     </div>
                                     {/*//item-inner*/}
@@ -87,8 +87,8 @@ const About = () => {
 
                                 <div className="item col-6 col-lg-3 with-margin">
                                     <div className="item-inner">
-                                        <div className="item-icon"><FontAwesomeIcon icon={faNpm} style={{color: "red", fontSize: 38 + "px"}} /></div>
-                                        <h4 className="item-title">npm</h4>
+                                        <div className="item-icon"><FontAwesomeIcon icon={faNpm} style={{ color: "red", fontSize: 38 + "px" }} /></div>
+                                        <h5 className="item-title">npm</h5>
 
                                     </div>
                                     {/*//item-inner*/}
@@ -97,8 +97,8 @@ const About = () => {
 
                                 <div className="item col-6 col-lg-3 with-margin">
                                     <div className="item-inner">
-                                        <div className="item-icon"><FontAwesomeIcon icon={faBootstrap} style={{color: "rgb(131, 64, 131)", fontSize: 30 + "px"}} /></div>
-                                        <h4 className="item-title">Bootstrap</h4>
+                                        <div className="item-icon"><FontAwesomeIcon icon={faBootstrap} style={{ color: "rgb(131, 64, 131)", fontSize: 30 + "px" }} /></div>
+                                        <h5 className="item-title">Bootstrap</h5>
 
                                     </div>
                                     {/*//item-inner*/}
@@ -107,8 +107,8 @@ const About = () => {
 
                                 <div className="item col-6 col-lg-3 with-margin">
                                     <div className="item-inner">
-                                        <div className="item-icon"><FontAwesomeIcon icon={faKorvue} style={{color: "purple", fontSize: 30 + "px"}} /></div>
-                                        <h4 className="item-title">Heroku</h4>
+                                        <div className="item-icon"><FontAwesomeIcon icon={faKorvue} style={{ color: "purple", fontSize: 30 + "px" }} /></div>
+                                        <h5 className="item-title">Heroku</h5>
 
                                     </div>
                                     {/*//item-inner*/}
@@ -117,8 +117,8 @@ const About = () => {
 
                                 <div className="item col-6 col-lg-3 with-margin">
                                     <div className="item-inner">
-                                        <div className="item-icon"><FontAwesomeIcon icon={faLeaf} style={{color: "green", fontSize: 30 + "px"}} /></div>
-                                        <h4 className="item-title">MongoDB</h4>
+                                        <div className="item-icon"><FontAwesomeIcon icon={faLeaf} style={{ color: "green", fontSize: 30 + "px" }} /></div>
+                                        <h5 className="item-title">MongoDB</h5>
 
                                     </div>
                                     {/*//item-inner*/}
@@ -128,7 +128,7 @@ const About = () => {
                                 <div className="item col-6 col-lg-3 with-margin">
                                     <div className="item-inner">
                                         <div className="item-icon"><img className=" mr-2" src={require("../assets/icons/MySQL.png").default} alt="MySql" /></div>
-                                        <h4 className="item-title">MySQL</h4>
+                                        <h5 className="item-title">MySQL</h5>
                                         <div className="item-desc">Experience with sequelize ORM</div>
 
                                     </div>
@@ -138,8 +138,8 @@ const About = () => {
 
                                 <div className="item col-6 col-lg-3 with-margin">
                                     <div className="item-inner">
-                                        <div className="item-icon"><img className="mr-5" src={require("../assets/icons/mustache.png").default }alt="mustache" style={{width: 35 + "px"}}/></div>
-                                        <h4 className="item-title">Handlebars</h4>
+                                        <div className="item-icon"><img className="mr-5" src={require("../assets/icons/mustache.png").default} alt="mustache" style={{ width: 35 + "px" }} /></div>
+                                        <h5 className="item-title">Handlebars</h5>
 
                                     </div>
                                     {/*//item-inner*/}
@@ -148,8 +148,8 @@ const About = () => {
 
                                 <div className="item col-6 col-lg-3 with-margin">
                                     <div className="item-inner">
-                                        <div className="item-icon"><img className="mr-5" src={require("../assets/icons/python.ico").default }alt="python" /></div>
-                                        <h4 className="item-title">Python</h4>
+                                        <div className="item-icon"><img className="mr-5" src={require("../assets/icons/python.ico").default} alt="python" /></div>
+                                        <h5 className="item-title">Python</h5>
                                         <div className="item-desc">Currently self-learning</div>
 
                                     </div>
@@ -158,9 +158,40 @@ const About = () => {
                             </div>
                             {/*//row*/}
 
-                        </p>
+                        </div>
                     </div>
                 </div>
+            </div>
+
+            <div className="education">
+
+                <div className="card text-center education">
+                    <h5 className="card-title">Profesional Experience & Education</h5>
+
+                    <div className="card-body">
+                        <br></br>
+                        <div className="card-text">
+                            {/* <div className="paragraphs"> */}
+                                <div className="row">
+
+                                    <div class="col-md-4">
+                                        <div className="clearfix content-heading">
+                                            <img className="pull-left edHead" src={require("../assets/images/edHead.png").default} alt="edHead.png" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <p>Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+                                    </div>
+                                </div>
+                                {/* </div> */}
+
+                            {/* </div> */}
+                            {/*//row*/}
+
+                        </div>
+                    </div>
+                </div>
+            
             </div>
         </React.Fragment>
     );
--- src/components/landing.js
+++ src/components/landing.js
@@ -9,14 +9,14 @@ const ShellTerminal = () => {
 
     return (
         <React.Fragment >
-            <div className="container">
+            <div className="wrapper">
+                
                 <div className="text-center" >
                     <img src={require("../assets/images/MikelColumn.png").default} className="rounded" alt="mikel" />
                 </div>
                 <div className="Typist bash"> 
                 <Navbar />
                     $Welcome to mikelrv's Shell! (V1.0.0)
-                    <br></br>
                     <br></br>
                     <br></br>
                     
@@ -29,8 +29,9 @@ const ShellTerminal = () => {
                                 cursor={{ hideWhenDone: true }}
                             >
                                 $ My name is Mikel Rodriguez. I'm a Full-Stack Developer and Spanish Linguist with a Master's degree in Iberian Linguistics. Welcome to my personal website!
-                                
+
                                 <Typist.Delay ms={250} />
+
                                 <br />
                                 <span>$ <a href={"https://www.linkedin.com/in/mikel-rodriguez-villar/"} className="flash">My Work</a></span>
                                 <br />
@@ -45,13 +46,11 @@ const ShellTerminal = () => {
                                 <span>$ <a href={"https://drive.google.com/file/d/1FXT9ZzMn6tvFQcJbAZbvFK_7vAgi1Cir/view"} className="flash" target="_blank" rel="noopener noreferrer">Resume</a></span>
                                 <br />
 
-
                                 
                             </Typist>
                         
                     </div>
                 </div>
-                <br></br>
                 <br></br>
                 <br></br>
             </div>
