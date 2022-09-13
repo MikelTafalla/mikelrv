6be92fac76806dfdfa0e1b8403259bd7292be2e8
Mikel <mikel362d@gmail.com>
2022-09-13 15:20:39 -0700

    changes about intro

--- src/components/aboutIntro.js
+++ src/components/aboutIntro.js
@@ -16,7 +16,10 @@ const AboutIntro = () => {
                     </div>
                     <div className="card-body">
                         <h5 className="card-title">Full-Stack Web Developer</h5>
-                        <p className="card-text">I am a Full-Stack Web Developer/ Software Engineer and a Spanish Linguist with an MA in Iberian Linguistics from the University of California Santa Barbara. I specialize in frontend and backend development for complex scalable web apps. I hope to join a learning-oriented company where I can promote growth as well as expand my own knowledge of programming. Want to find out more about my experience? Check out my online resume and project portfolio.</p>
+                        <p className="card-text">I am a Full-Stack Web Developer/ Software Engineer who
+                         specializes in frontend and backend development for complex scalable web apps.<br></br> 
+                         {/* I hope to join a learning-oriented company where I can promote growth as well as expand my own knowledge of programming.  */}
+                         Want to find out more about my experience? Check out my online resume and project portfolio.</p>
                         <Link to="/portfolio" className="btn btn-light">My Work</Link>
                         <a href="https://drive.google.com/file/d/1FXT9ZzMn6tvFQcJbAZbvFK_7vAgi1Cir/view" className="btn btn-dark" target="_blank" rel="noopener noreferrer">Resume</a>
                     </div>
